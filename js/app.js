e53d3164c5af75c29fb442ff873019f0f15bac0e
GonzaloCaldoli <120001377+GonzaloCaldoli@users.noreply.github.com>
2022-12-09 03:06:42 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const dropdowns = document.querySelectorAll( '.dropdown1' ) ;
+dropdowns.forEach(dropdown1 =>{
+    const select = dropdown1.querySelector('.select');
+    const caret = dropdown1.querySelector('.caret');
+    const menu = dropdown1.querySelector( ' .menu ' ) ;
+    const options = dropdown1.querySelectorAll(' .menu li' ) ;
+    const selected= dropdown1.querySelector( ' .selected' ) ;
+
+    select.addEventListener('click', ()=>{
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    })
+
+} )
+
+    options.forEach(option=>{
+        option.addEventListener('click',()=>{
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open')
+            options.forEach(option =>{
+                option.classList.remove('active');
+            })
+        })
+    })
